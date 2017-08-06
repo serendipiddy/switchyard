4db5c3636e2e8560e2bdfd8bc6ae46fc2276f5e9
iddy@n80 <serendipiddy@gmail.com>
2017-08-07 02:07:23 +1200

    added hacks to ICMPv6 to retain type value after call to super, started working on ICMPv6 NeighborAdvetisement message

--- switchyard/lib/packet/icmpv6.py
+++ switchyard/lib/packet/icmpv6.py
@@ -16,22 +16,25 @@ References:
 
 class ICMPv6(ICMP):
     def __init__(self, **kwargs):
+        # Another hacky way to make this thing work.. super should be last..
+        self.icmp6type = kwargs['icmptype']
+        del kwargs['icmptype']
+        super().__init__(**kwargs)
+        kwargs['icmptype'] = self.icmp6type
+        
         self._valid_types = ICMPv6Type
         self._valid_codes_map = ICMPv6TypeCodeMap
         self._classtype_from_icmptype = ICMPv6ClassFromType
         self._icmptype_from_classtype = ICMPv6TypeFromClass
         self._type = self._valid_types.EchoRequest
         self._code = self._valid_codes_map[self._type].EchoRequest
-        print("type: {}".format(self._type))
         self._icmpdata = ICMPv6ClassFromType(self._type)()
-        print("icmpdata: {}".format(self._icmpdata))
         self._checksum = 0
         # if kwargs are given, must ensure that type gets set
         # before code due to dependencies on validity.
         if 'icmptype' in kwargs:
             self.icmptype = kwargs['icmptype']
-            del kwargs['icmptype']
-        super().__init__(**kwargs)
+            # del kwargs['icmptype']
 
     def checksum(self):
         return self._checksum
@@ -54,6 +57,8 @@ class ICMPv6Data(ICMPData):
     '''Hack to make the inheritance chain happy and lead into v6 specific differences'''
     pass
 
+class ICMPv6Options(ICMPv6Data):
+    pass
 
 class ICMPv6EchoRequest(ICMPEchoRequest):
     pass
@@ -75,9 +80,41 @@ class ICMPv6MobilePrefixAdvertisement(ICMPv6Data):
     pass
 
 class ICMPv6NeighborSolicitation(ICMPv6Data):
-    pass
+    # pass
+    __slots__ = ['_targetaddr']
+    _PACKFMT = "!xxxx16s"
+    '''
+        possible options:
+          * source_link_layer_address: link layer address of sending host
+    '''
 
-#class ICMPv6NeighborAdvertisement(ICMPv6Data):
+    def __init__(self, **kwargs):
+        self._targetaddr = IPv6Address("::0")
+        super().__init__(**kwargs)
+
+    def to_bytes(self):
+        # return b''.join(super().to_bytes())
+        return b''.join( (struct.pack(ICMPv6NeighborSolicitation._PACKFMT, self._targetaddr.packed), super().to_bytes()) )
+
+    def from_bytes(self):
+        pass
+
+    @property
+    def targetaddr(self):
+        return self._targetaddr
+
+    @targetaddr.setter
+    def targetaddr(self, value):
+        print("setting target address: {}".format(IPv6Address(value)))
+        self._targetaddr = IPv6Address(value)
+
+class ICMPv6NeighborAdvertisement(ICMPv6Data):
+    __slots__ = ['_R_S_O','_targetaddr']
+    _PACKFMT = "!cxxx16s"
+    '''
+        possiblie options:
+          * source_link_layer_address: link layer address of sending host
+    '''
     pass
 
 def construct_icmpv6_class_map():
