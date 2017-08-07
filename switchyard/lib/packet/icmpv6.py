6f3f86302fd1ff11e428faa3fb03f4021bd905da
anselljord <anselljord@ecs.vuw.ac.nz>
2017-08-07 13:46:59 +1200

    ICMPv6 Neighbor Solicitation/Advertisement classes now pack/unpack, no longer have '0 byte' __str__ and ICMPv6 class works when no kwargs[icmptype]

--- switchyard/lib/packet/icmpv6.py
+++ switchyard/lib/packet/icmpv6.py
@@ -5,6 +5,7 @@ from .icmp import ICMP, ICMPData, ICMPEchoRequest, ICMPEchoReply
 from .common import ICMPv6Type, ICMPv6TypeCodeMap
 from .common import checksum as csum
 from ..exceptions import *
+from sys import byteorder
 
 '''
 References:
@@ -17,10 +18,12 @@ References:
 class ICMPv6(ICMP):
     def __init__(self, **kwargs):
         # Another hacky way to make this thing work.. super should be last..
-        self.icmp6type = kwargs['icmptype']
-        del kwargs['icmptype']
+        if 'icmptype' in kwargs:
+            self.icmp6type = kwargs['icmptype']
+            del kwargs['icmptype']
         super().__init__(**kwargs)
-        kwargs['icmptype'] = self.icmp6type
+        if hasattr(self, "icmp6type"):
+            kwargs['icmptype'] = self.icmp6type
         
         self._valid_types = ICMPv6Type
         self._valid_codes_map = ICMPv6TypeCodeMap
@@ -80,9 +83,9 @@ class ICMPv6MobilePrefixAdvertisement(ICMPv6Data):
     pass
 
 class ICMPv6NeighborSolicitation(ICMPv6Data):
-    # pass
     __slots__ = ['_targetaddr']
     _PACKFMT = "!xxxx16s"
+    _MINLEN = struct.calcsize(_PACKFMT)
     '''
         possible options:
           * source_link_layer_address: link layer address of sending host
@@ -93,11 +96,13 @@ class ICMPv6NeighborSolicitation(ICMPv6Data):
         super().__init__(**kwargs)
 
     def to_bytes(self):
-        # return b''.join(super().to_bytes())
         return b''.join( (struct.pack(ICMPv6NeighborSolicitation._PACKFMT, self._targetaddr.packed), super().to_bytes()) )
 
-    def from_bytes(self):
-        pass
+    def from_bytes(self, raw):
+        if len(raw) < self._MINLEN:
+            raise NotEnoughDataError("Not enough bytes to unpack ICMPv6NeighborSolicitation object")
+        fields = struct.unpack(ICMPv6NeighborSolicitation._PACKFMT, raw)
+        self._targetaddr = IPv6Address(fields[0])
 
     @property
     def targetaddr(self):
@@ -107,15 +112,78 @@ class ICMPv6NeighborSolicitation(ICMPv6Data):
     def targetaddr(self, value):
         print("setting target address: {}".format(IPv6Address(value)))
         self._targetaddr = IPv6Address(value)
+    
+    def __str__(self):
+        return ''
 
 class ICMPv6NeighborAdvertisement(ICMPv6Data):
     __slots__ = ['_R_S_O','_targetaddr']
     _PACKFMT = "!cxxx16s"
+    _MINLEN = struct.calcsize(_PACKFMT)
     '''
-        possiblie options:
+        possible options:
           * source_link_layer_address: link layer address of sending host
     '''
-    pass
+    def __init__(self, **kwargs):
+        self._targetaddr = IPv6Address("::0")
+        self._routerflag = 0
+        self._solicitedflag = 0
+        self._overrideflag = 0
+        super().__init__(**kwargs)
+        
+    def to_bytes(self):
+        rso = self._routerflag << 7 | self._solicitedflag << 6 | self._overrideflag << 5 
+        rso_byte = int.to_bytes(rso, length=1, byteorder=byteorder, signed=False)
+        return b''.join( (struct.pack(ICMPv6NeighborAdvertisement._PACKFMT, rso_byte, self._targetaddr.packed), super().to_bytes()) )
+
+    def from_bytes(self, raw):
+        if len(raw) < self._MINLEN:
+            raise NotEnoughDataError("Not enough bytes to unpack ICMPv6NeighborAdvertisement object")
+        fields = struct.unpack(ICMPv6NeighborSolicitation._PACKFMT, raw)
+        rso = int.from_bytes(fields[0], byteorder=byteorder, signed=False)
+        self._routerflag = (rso & 0x80) >> 7
+        self._solicitedflag = (rso & 0x40) >> 6
+        self._overrideflag = (rso & 0x20) >> 5
+        self._targetaddr = IPv6Address(fields[0])
+
+    @property
+    def targetaddr(self):
+        return self._targetaddr
+
+    @targetaddr.setter
+    def targetaddr(self, value):
+        self._targetaddr = IPv6Address(value)
+    
+    @property
+    def routerflag(self):
+        return bool(self._routerflag)
+    
+    @property
+    def solicitedflag(self):
+        return bool(self._solicitedflag)
+    
+    @property
+    def overrideflag(self):
+        return bool(self._overrideflag)
+      
+    @routerflag.setter
+    def routerflag(self, value):
+        assert value == True or value == False
+        self._routerflag = int(value)
+    
+    @solicitedflag.setter
+    def solicitedflag(self, value):
+        assert value == True or value == False
+        self._solicitedflag = int(value)
+    
+    @overrideflag.setter
+    def overrideflag(self, value):
+        assert value == True or value == False
+        self._overrideflag = int(value)
+    
+    def __str__(self):
+        return ''    
+    
 
 def construct_icmpv6_class_map():
     clsmap = {}
